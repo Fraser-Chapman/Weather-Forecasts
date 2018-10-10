522300922129b156ce8cad929df14320949bbd07
Fraser-Chapman <fraserchapman16@gmail.com>
2018-10-10 16:07:47 +0100

    added picture code, as well as minimum and maximum temp for the day

--- src/components/forecasts.js
+++ src/components/forecasts.js
@@ -11,6 +11,10 @@ function Forecast(forecast) {
     return `
         <div class = "forecast">
             <h4>${forecast.date}</h4>
+            <p>${forecast.icon}</p>
+            <p>${forecast.description}</p>
+            <p>High of: ${forecast.temperature.max} &deg;C</p>
+            <p>Low of: ${forecast.temperature.min} &deg;C</p>
         </div>
     
     `;
